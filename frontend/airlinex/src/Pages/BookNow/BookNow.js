598a996c261a9fc71d767e019f4ff2e7aeb62fcc
OmkarRane <omkar.m.rane.30@gmail.com>
2022-09-27 11:28:27 +0530

    updated book now

--- frontend/airlinex/src/Pages/BookNow/BookNow.js
+++ frontend/airlinex/src/Pages/BookNow/BookNow.js
@@ -2,23 +2,54 @@ import "./BookNow.css";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Sidebar from "../../Components/Sidebar/Sidebar";
 import Card from "./Card";
+import Dropdown from "./Dropdown";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 const BookNow = () => {
+  const fromOptions = ["Mumbai", "Delhi", "New York", "Boston"];
+  const toOptions = ["Andheri", "Delhi", "New York", "Boston"];
+
   return (
     <Sidebar>
       <h1>Book Now</h1>
       <Card>
-        <div className="tripInput">
+        <div className="tripInputPrim">
           <div>
-            <input type="radio" id="one-way-trip"></input>
-            <label htmlFor="one-way-trip">one-way-trip</label>
+            <input type="radio" name="trip_type" id="one-way-trip"></input>
+            <label htmlFor="one-way-trip">One-way-trip</label>
           </div>
+          <div className="spacer"></div>
           <div>
-            <input type="radio" id="two-way-trip"></input>
-            <label htmlFor="two-way-trip">one-way-trip</label>
+            <input type="radio" name="trip_type" id="two-way-trip"></input>
+            <label htmlFor="two-way-trip">Two-way-trip</label>
           </div>
         </div>
         <hr></hr>
-        <div>Hello</div>
+        <div>
+          <div className="tripInputRow">
+            <div className="tripInputCol">
+              <label for="myFromDropdown">From</label>
+              <Dropdown id="myFromDropdown" options={fromOptions}>
+                Choose source
+              </Dropdown>
+            </div>
+            <div className="tripInputCol">
+              <label for="myToDropdown">To</label>
+              <Dropdown id="myToDropdown" options={toOptions}>
+                Choose destination
+              </Dropdown>
+            </div>
+            <div className="tripInputCol">
+              <label for="departDate">Depart</label>
+              <input type="date" id="departDate"></input>
+            </div>
+            <div className="tripInputCol">
+              <label for="arriveDate">Arrive</label>
+              <input type="date" id="arriveDate"></input>
+            </div>
+          </div>
+        </div>
+        <div></div>
       </Card>
     </Sidebar>
   );
