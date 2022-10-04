5dbd317912897b42a4ddac3d3c429248cf2f01c8
Keyur <keyur.pancholi9@gmail.com>
2022-10-04 11:36:15 +0530

    Added footer

---
+++ frontend/airlinex/src/Components/Footer/Footer.js
@@ -0,0 +1,89 @@
+// import React from "react";
+import {
+	Box,
+	Container,
+	Row,
+	Column,
+	FooterLink,
+	Heading,
+} from "./FooterStyles";
+
+const Footer = () => {
+	return (
+		<Box>
+			<Container>
+				<Row>
+					<Column>
+						<Heading>About Us</Heading>
+						<FooterLink href="#">Airlinex Green</FooterLink>
+						<FooterLink href="#">Board of Directors</FooterLink>
+						<FooterLink href="#">Leadership Team</FooterLink>
+						<FooterLink href="#">Investor Relations</FooterLink>
+						<FooterLink href="#">Press Releases</FooterLink>
+						<FooterLink href="#">Transgender Persons Policy</FooterLink>
+						<FooterLink href="#">Our Awards</FooterLink>
+					</Column>
+					<Column>
+						<Heading>Services</Heading>
+						<FooterLink href="#">Special Disability Assistance</FooterLink>
+						<FooterLink href="#">Medical Assistance</FooterLink>
+						<FooterLink href="#">Seat Select</FooterLink>
+						<FooterLink href="#">Gift Voucher</FooterLink>
+						<FooterLink href="#">6E Tiffin</FooterLink>
+						<FooterLink href="#">Add-ons & Services</FooterLink>
+						<FooterLink href="#">Baggage</FooterLink>
+					</Column>
+					<Column>
+						<Heading>Quick Links</Heading>
+						<FooterLink href="#">Offers</FooterLink>
+						<FooterLink href="#">Careers</FooterLink>
+						<FooterLink href="#">Advertise with us</FooterLink>
+						<FooterLink href="#">Destinations</FooterLink>
+						<FooterLink href="#">Terms and Conditions</FooterLink>
+						<FooterLink href="#">Conditions of Carriage</FooterLink>
+						<FooterLink href="#">Privacy Policy</FooterLink>
+						<FooterLink href="#">Disclaimer</FooterLink>
+						<FooterLink href="#">International Travel Tips</FooterLink>
+					</Column>
+					<Column>
+						<Heading>Social Media</Heading>
+						<FooterLink href="#">
+							<i className="fab fa-facebook-f">
+								<span style={{ marginLeft: "10px" }}>
+									Facebook
+								</span>
+							</i>
+						</FooterLink>
+						<FooterLink href="#">
+							<i className="fab fa-instagram">
+								<span style={{ marginLeft: "10px" }}>
+									Instagram
+								</span>
+							</i>
+						</FooterLink>
+						<FooterLink href="#">
+							<i className="fab fa-twitter">
+								<span style={{ marginLeft: "10px" }}>
+									Twitter
+								</span>
+							</i>
+						</FooterLink>
+						<FooterLink href="#">
+							<i className="fab fa-youtube">
+								<span style={{ marginLeft: "10px" }}>
+									Youtube
+								</span>
+							</i>
+						</FooterLink>
+					</Column>
+				</Row>
+				<br />
+				<Row>
+					&copy; 2022 Airlinex. All rights reserved.
+				</Row>
+				
+			</Container>
+		</Box>
+	);
+};
+export default Footer;
--- frontend/airlinex/src/Pages/Login/Login.js
+++ frontend/airlinex/src/Pages/Login/Login.js
@@ -2,34 +2,68 @@ import { useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./Login.css";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faGoogle, faFacebook, faGithub} from '@fortawesome/free-brands-svg-icons'
+
 
 const LoginPage = () => {
+  const email = useRef();
+  const password = useRef();
 
-  const email = useRef()
-  const password = useRef()
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
-
-  const loginHandler = e => {
+  const loginHandler = (e) => {
     e.preventDefault();
-    console.log(email.current.value, password.current.value)
-    navigate('/')
-  }
+    console.log(email.current.value, password.current.value);
+    navigate("/");
+  };
 
   return (
     <div className="login-outer-container">
       <div className="login-container">
-        <h2>Login</h2>
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" placeholder="Type your email" ref={email} />
-        <label htmlFor="password">Password</label>
-        <input type="password" placeholder="Type your password" ref={password} />
-        <button className="btn btn-success" type="submit"  onClick={loginHandler}>
-          Login
+        <div className="login-heading">
+          <h2>Airlinex</h2>
+        </div>
+        <div className="login-email">
+          <input
+            type="text"
+            placeholder="Email"
+            className="hello"
+            ref={email}
+          />
+        </div>
+
+        <span className="login-seperator"></span>
+
+        <div className="login-password">
+          <input
+            type="password"
+            placeholder="Password"
+            className="hello"
+            ref={password}
+          />
+        </div>
+
+        <span className="login-seperator"></span>
+
+        <div className="login-fa-icons">
+          <div className="login-fa-item">
+          <FontAwesomeIcon icon={faGoogle} color="white" height="10px"></FontAwesomeIcon>
+          </div>
+          <div className="login-fa-item">
+            <FontAwesomeIcon icon={faFacebook} color="white"></FontAwesomeIcon>
+          </div>
+          <div className="login-fa-item">
+            <FontAwesomeIcon icon={faGithub} color="white"></FontAwesomeIcon>
+          </div>
+        </div>
+
+        <div className="login-submit-container">
+        <button className="loginbtn" onClick={loginHandler}>
+            Log in
         </button>
-        <p>
-          Don't have an account? <Link to="/signup">Sign in</Link>
-        </p>
+
+        </div>
       </div>
     </div>
   );
